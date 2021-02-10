c1056d0b04ea8af11ade02fbad055745bb900f39
Lucas <lucaspdroz@gmail.com>
2021-02-10 16:49:48 -0300

    exemplo pro will 7

--- src/App.js
+++ src/App.js
@@ -7,20 +7,20 @@ import {
 import './global.scss';
 import Home from './Pages/Home';
 import About from './Pages/About';
-// import Contact from './Pages/Contact';
-// import Navbar from './Components/Navbar/Navbar';
+import Contact from './Pages/Contact';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <Router>
-      {/* <Navbar /> */}
+      <Navbar />
       <Switch>
         <Route path="/about">
           <About />
         </Route>
-        {/* <Route path="/contact">
+        <Route path="/contact">
           <Contact />
-        </Route> */}
+        </Route>
         <Route exatc path="/">
           <Home title="Um titulo passado por props" />
         </Route>
