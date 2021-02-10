12161b3dc75be7a0423b61b82b22a74b6bdb2d9f
Lucas <lucaspdroz@gmail.com>
2021-02-09 23:53:43 -0300

    failed compile

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import {
   Route
 } from 'react-router-dom';
 
-import Navbar from './components/Navbar/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
