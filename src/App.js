bac2cceeae8cec65bff36a9f059bdb1d429628f9
Lucas <lucaspdroz@gmail.com>
2021-02-09 23:46:43 -0300

    homepath

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import {
   Route
 } from 'react-router-dom';
 
-import Navbar from './components/Navbar/Navbar'
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
